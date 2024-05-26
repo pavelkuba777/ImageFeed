dae1fc59544e9cab30965c21d68e2da575485010
pavelkuba777 <p.cuba@yandex.ru>
2024-05-26 12:39:47 +0200

    Создание профиля

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Павел Куба on 25.05.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+    
+}
