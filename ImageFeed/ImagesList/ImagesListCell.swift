3d38adeef9636b31323ea688541e762f2a33ad50
pavelkuba777 <p.cuba@yandex.ru>
2024-06-23 21:12:40 +0200

    Добавление скролла и зума, вёрстка кодом окна профиля.

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -10,9 +10,22 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     
+    // MARK: - Public properties
     static let reuseIdentifier = "ImagesListCell"
     
+    // MARK: - Outlets
     @IBOutlet var cellImage: UIImageView!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
+}
+
+// MARK: - Extansion
+extension ImagesListCell {
+    func config(image: UIImage?, date: String, isLiked: Bool) {
+        let likedImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        
+        cellImage.image = image
+        dateLabel.text = date
+        likeButton.setImage(likedImage, for: .normal)
+    }
 }
--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -8,15 +8,123 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-    
-    @IBOutlet private var logoutButton: UIButton!
-    
-    @IBAction private func didTapLogoutButton() {
+    // MARK: - Private properties
+    private var profilePhotoImage = UIImageView()
+    private var profileFullNameLabel = UILabel()
+    private var profileLoginNameLabel = UILabel()
+    private var profileDescLabel = UILabel()
+    private var exitButton = UIButton()
+
+    // MARK: - Mock data
+    private let profilePhoto = "Photo"
+    private let profileUserName = "Екатерина Новикова"
+    private let profileLoginName = "@ekaterina_nov"
+    private let profileDescription = "Hello, World!"
+
+    // MARK: - Public properties
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+      return .lightContent
     }
-    
-}
+
+    // MARK: - Lifecyrcle
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      makeProfilePhotoImage()
+      makeProfileFullNameLabel()
+      makeProfileLoginNameLabel()
+      makeProfileBioLabel()
+      makeProfileExitButton()
+    }
+  }
+
+  // MARK: - Private methods
+
+  private extension ProfileViewController {
+    @objc func didTapButton() {
+    }
+
+    func makeProfilePhotoImage() {
+      profilePhotoImage.image = UIImage(named: profilePhoto)
+      profilePhotoImage.translatesAutoresizingMaskIntoConstraints = false
+
+      view.addSubview(profilePhotoImage)
+
+      NSLayoutConstraint.activate([
+        profilePhotoImage.widthAnchor.constraint(equalToConstant: 70),
+        profilePhotoImage.heightAnchor.constraint(equalToConstant: 70),
+        profilePhotoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+        profilePhotoImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
+      ])
+    }
+
+    func makeProfileFullNameLabel() {
+      profileFullNameLabel.text = profileUserName
+      profileFullNameLabel.textColor = .ypWhite
+      profileFullNameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+      profileFullNameLabel.lineBreakMode = .byWordWrapping
+      profileFullNameLabel.numberOfLines = 2
+
+      profileFullNameLabel.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(profileFullNameLabel)
+
+      NSLayoutConstraint.activate([
+        profileFullNameLabel.topAnchor.constraint(equalTo: profilePhotoImage.bottomAnchor, constant: 8),
+        profileFullNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        profileFullNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+      ])
+    }
+
+    func makeProfileLoginNameLabel() {
+      profileLoginNameLabel.text = profileLoginName
+      profileLoginNameLabel.textColor = .ypGray
+      profileLoginNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+
+      profileLoginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(profileLoginNameLabel)
+
+      NSLayoutConstraint.activate([
+        profileLoginNameLabel.topAnchor.constraint(equalTo: profileFullNameLabel.bottomAnchor, constant: 8),
+        profileLoginNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        profileLoginNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+      ])
+    }
+
+    func makeProfileBioLabel() {
+      profileDescLabel.text = profileDescription
+      profileDescLabel.textColor = .ypWhite
+      profileDescLabel.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+
+      profileDescLabel.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(profileDescLabel)
+
+      NSLayoutConstraint.activate([
+        profileDescLabel.topAnchor.constraint(equalTo: profileLoginNameLabel.bottomAnchor, constant: 8),
+        profileDescLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        profileDescLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+      ])
+    }
+
+    func makeProfileExitButton() {
+      let profileExitButtonImage = UIImage(named: "Exit") ?? UIImage()
+
+      exitButton = UIButton.systemButton(
+        with: profileExitButtonImage,
+        target: self,
+        action: #selector(self.didTapButton)
+      )
+      exitButton.tintColor = .ypRed
+
+      exitButton.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(exitButton)
+
+      NSLayoutConstraint.activate([
+        exitButton.widthAnchor.constraint(equalToConstant: 44),
+        exitButton.heightAnchor.constraint(equalToConstant: 44),
+        exitButton.centerYAnchor.constraint(equalTo: profilePhotoImage.centerYAnchor),
+        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+      ])
+    }
+  }
